f379f7c43e0a4b8188a07c589b912011dabd3fa6
Phongphisut0812 <68526077+Phongphisut0812@users.noreply.github.com>
2020-09-25 22:23:16 +0700

    new

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/h3.png">
+    <HelloWorld msg="Welcome to Your Dictionary Search"/>
+    <anime />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Anime from'./components/Anime.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Anime
   }
 }
 </script>
---
+++ src/components/Anime.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="containr">
+    <!-- <input type="text" v-model="keyword" /> -->
+    <button class="btn btn-outline-warning " @click="searchData()">Search </button>
+    {{result}} 
+
+     
+        <b-card
+          v-if = "result" 
+          :title="result.word"
+          tag="article"
+          style="max-width: 20rem;"
+          class="mb-2"
+        >
+          <b-card-text>{{data.meanings.definitions.definition}}</b-card-text> 
+
+          <b-button href="data.url" variant="primary">Go somewhere</b-button>
+        </b-card>
+       
+    </div>
+   
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      result: null,
+      keyword: "",
+    };
+  },
+  methods: {
+    searchData() {
+      axios
+        .get("https://api.dictionaryapi.dev/api/v2/entries/en/hello" )
+        .then((response) => {
+          this.result = response.data;
+           console.log(this.result);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
